83ec2c8a7ccc55fac2f64e44e09ff35f8a464992
Nakamura Takahiro <takahiro.nakamura@gi-no.jp>
2017-04-29 22:57:21 +0900

    add some log

--- scripts/forward_geekbot_broadcast.js
+++ scripts/forward_geekbot_broadcast.js
@@ -4,12 +4,13 @@ module.exports = (robot => {
     async (msg) => {
       const dataStore = robot.adapter.client.rtm.dataStore
       const room = await dataStore.getChannelById(msg.envelope.room)
-      robot.logger.info `room name is: ${ room.name }`
+      robot.logger.info(`room name is: ${ room.name }`)
+      robot.logger.info(msg.message.text)
 
       // if (!room || room.name != 'test' ) { return }
       if (! msg.message.text.match('takahiro nakamura')) { return }
 
-      robot.logger.info `room name is: ${ room.name }`
+      robot.logger.info(`room name is: ${ room.name }`)
 
       robot.send(
         { room: 'test' },
