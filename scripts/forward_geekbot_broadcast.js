4c9df2c61546c4334447085e6b6a61a6c4811d5d
Nakamura Takahiro <takahiro.nakamura@gi-no.jp>
2017-04-29 18:21:21 +0900

    fix syntax

--- scripts/forward_geekbot_broadcast.js
+++ scripts/forward_geekbot_broadcast.js
@@ -2,7 +2,7 @@ module.exports = (robot => {
   robot.respond(
     /.+/,
     async (msg) => {
-      dataStore = robot.adapter.client.rtm.dataStore
+      const dataStore = robot.adapter.client.rtm.dataStore
       const room = async  dataStore.getChannelById(msg.envelope.room)
       const user = async  dataStore.getUserById(msg.envelope.user)
       // if (!room || room.name != 'test' ) { return }
