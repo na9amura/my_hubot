80e5f6b4cf876142edd6c3e660448b96d0492bff
Nakamura Takahiro <takahiro.nakamura@gi-no.jp>
2017-04-29 23:27:06 +0900

    change hear condition regexp

--- scripts/forward_geekbot_broadcast.js
+++ scripts/forward_geekbot_broadcast.js
@@ -1,22 +1,14 @@
 module.exports = (robot => {
   robot.hear(
-    /.+/,
+    /posted a status/,
     async (msg) => {
-      robot.logger.info('===================')
-
       const dataStore = robot.adapter.client.rtm.dataStore
       const room = await dataStore.getChannelById(msg.envelope.room)
 
-      robot.logger.info(`room name is: ${ room.name }`)
-      robot.logger.info(msg.message.text)
-
-      // if (!room || room.name != 'test' ) { return }
-      if (! msg.message.text.match('takahiro nakamura')) { return }
-
-      robot.logger.info(`room name is: ${ room.name }`)
+      if (!room || room.name != 'test' ) { return }
 
       robot.send(
-        { room: 'test' },
+        { room: '#test' },
         msg.message.text
       )
     }
